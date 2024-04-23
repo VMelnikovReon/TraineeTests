d3c4a530167aacf318f43169177291928ccdd0d5
VMelnikovReon <vmelnikov.reon@gmail.xom>
2024-04-23 16:00:18 +0300

    refactor

--- infrastructure/consts.ts
+++ infrastructure/consts.ts
@@ -1,7 +1,5 @@
 export const CUSTOM_FIELDS_ID = {
 	CONTACT:{
-		BIRTHDAY_DATE: 785303,
-		AGE: 930767,
 		ULTRASONIC_LIFTING: 878645,
 		LAZER_REJUVENATION: 878647,
 		LAZER_REMOVAL: 878697,
@@ -25,7 +23,6 @@ export const CUSTOM_FIELDS_ID = {
 export const ROUTES = {
 	HOOKS: {
 		HOME_ROUTE: '/hooks',
-		ADD_CONTACR_ROUTE : '/addcontact',
 		UPDATE_DEAL_ROUTE : '/updatedeal'
 	},
 	PING_ROUTE: '/ping'
