37052eade3d70c56221560543fc8719fc65088ed
vladislavReon <vmelnikov.reon@gmail.com>
2024-04-24 14:19:19 +0300

    refactor

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import { CustomApplication } from "./CustomApp";
+import mongoose from "mongoose";
 
 const api = require("./api/api");
 const app = new CustomApplication();
+
+const connectionString = 'mongodb://127.0.0.1:27017/traineTest';
+
+mongoose.connect(connectionString)
+	.then(()=>console.log('connect'))
+	.catch((err)=>console.log(err));
 
 app
 	.addServices()
