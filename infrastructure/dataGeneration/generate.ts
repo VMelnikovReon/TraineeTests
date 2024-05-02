f94d9302659079762a09397faf20acbb32a4a1aa
VMelnikovReon <vmelnikov.reon@gmail.xom>
2024-05-02 16:14:24 +0300

    refactor

--- infrastructure/dataGeneration/generate.ts
+++ infrastructure/dataGeneration/generate.ts
@@ -5,39 +5,36 @@ import { VisiteModel } from '../../models/VisiteModel';
 import { ObjectId } from 'mongoose';
 
 export async function generateClients(count: number) : Promise<void> {
-    const clients = [];
-    for (let i = 0; i < count; i++) {
+    const clients = Array.from({length : count}, (_item, index)=>{
         const client = new ClientModel({
-            fname: 'Name' + i,
-            lname: 'Lastname' + i,
-            patronymic: 'Patronymic' + i,
-            birthDate: new Date(i),
-            phone: '123456789' + i
+            fname: 'Name' + index,
+            lname: 'Lastname' + index,
+            patronymic: 'Patronymic' + index,
+            birthDate: new Date(index),
+            phone: '123456789' + index
         });
-        clients.push(client);
-    }
+        return client;
+    })
     await ClientModel.insertMany(clients);
 }
 
 export async function generateServices(count: number): Promise<void> {
-    const services = [];
-    for (let i = 0; i < count; i++) {
+    const services = Array.from({length: count}, (_item,index)=>{
         const service = new ServiceModel({
-            name: 'Service' + i,
-            code: 'CODE' + i,
+            name: 'Service' + index,
+            code: 'CODE' + index,
             price: Math.floor(Math.random() * 100) + 50,
-            description: 'Description of service ' + i
+            description: 'Description of service ' + index
         });
-        services.push(service);
-    }
+        return service;
+    });
     await ServiceModel.insertMany(services);
 }
 
 export async function generateVisits(count: number, clients : Client[], services: Service[]): Promise<void> {
-    const visits = [];
     const statuses = ['planned', 'visited', 'cancelled'];
 
-    for (let i = 0; i < count; i++) {
+    const visits = Array.from({length: count}, (_item, index)=>{
         const status = statuses[Math.floor(Math.random() * (statuses.length - 1))];
 
 		const serviseCount = Math.random() * (services.length - 1);
@@ -46,7 +43,7 @@ export async function generateVisits(count: number, clients : Client[], services
 
 		let totalPrice = 0;
 
-		for (let i = 0; i<= serviseCount; i++){
+		for (let j = 0; j<= serviseCount; j++){
 			const service = services[Math.floor(Math.random() * (services.length -1))];
 
 			if (visitServices.includes(service._id)){
@@ -61,14 +58,13 @@ export async function generateVisits(count: number, clients : Client[], services
 
         const visit = new VisiteModel({
             client: randomClient._id,
-            plannedDateTime: new Date(i),
-            actualDateTime: status === 'visited' ? new Date(i+1) : null,
+            plannedDateTime: new Date(index),
+            actualDateTime: status === 'visited' ? new Date(index+1) : null,
             status: status,
             services: status === 'visited' ? visitServices : null,
             totalCost: status === 'visited' ? totalPrice : null
         });
-        visits.push(visit);
-
-    }
+        return visit;
+    })
     await VisiteModel.insertMany(visits);
 }
